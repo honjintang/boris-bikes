245849db159f491a949b3b884b6c134774e95084
Freddy Fallon <freddyfallon@gmail.com>
2017-03-15 15:30:58 +0000

    Adding exercie 12

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,17 @@
 require 'docking_station'
 describe DockingStation do
-  it { is_expected.to respond_to(:release_bike) }
+
+  describe '#release_bike' do
+      it 'releases a bike' do
+        bike = Bike.new
+        subject.dock(bike)
+        expect(subject.release_bike).to eq bike
+      end
+
+      it 'raises an error when bike is released' do
+       expect { subject.release_bike }.to raise_error('No bikes available')
+      end
+  end
 
   it { is_expected.to respond_to(:dock).with(1).argument }
 
@@ -8,6 +19,8 @@ describe DockingStation do
 
 
   it 'releases working bikes' do
+    bike = Bike.new
+    subject.dock(bike)
     bike = subject.release_bike
     expect(bike).to be_working
   end
@@ -22,4 +35,6 @@ describe DockingStation do
     subject.dock(bike)
     expect(subject.bike).to eq bike
   end
+
+
 end
