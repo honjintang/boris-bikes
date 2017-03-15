1156b21891380d800767490bb1b4094db2f7eafd
Freddy Fallon <freddyfallon@gmail.com>
2017-03-15 15:42:45 +0000

    Making further changes to resolves conflicts

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,13 +1,6 @@
 require_relative 'bike'
 
 class DockingStation
-
-  attr_reader :bike
-
-  def initialize
-    @bike = nil
-  end
-
   def release_bike
     fail 'No bikes available' unless @bike
     @bike
